a8c88ddd00aa87fc288e347b4684753901f6334d
Liam Leung <liam.leung@binery.co>
2023-09-14 17:28:08 +0700

    chore: rm deprecated folder

--- packages/blog-frontend-2/src/hooks/use-api-service.ts
+++
@@ -1,5 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { useContext } from "react";
-import { ApiContext } from "../contexts/api-context";
-
-export const useApi = () => useContext(ApiContext) as any;
--- packages/blog-frontend-2/src/hooks/use-auth.ts
+++
@@ -1,5 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { useContext } from 'react';
-import { AuthContext } from '../contexts/auth-context';
-
-export const useAuth = () => useContext(AuthContext) as any;
