4bbd3ecc4a1ab1f02f46d2ae862e2c2b120c2309
Liam Leung <liam.leung@binery.co>
2023-09-14 12:03:47 +0700

    setup: initial packages + eslint

---
+++ packages/blog-backend/sequelize.config.js
@@ -0,0 +1,89 @@
+require('dotenv').config();
+
+let dialectOptions = {};
+
+if (process.env.NODE_ENV !== 'local' && process.env.NODE_ENV !== 'test') {
+  dialectOptions = {
+    ssl: {
+      host: process.env.DB_HOST,
+      ca: Buffer.from(process.env.POSTGRES_SSL_SERVER_CA, 'base64'),
+      key: Buffer.from(process.env.POSTGRES_SSL_CLIENT_KEY, 'base64'),
+      cert: Buffer.from(process.env.POSTGRES_SSL_CLIENT_CERT, 'base64'),
+      rejectUnauthorized: false,
+      requestCert: true,
+    },
+  };
+}
+
+let config = {
+  local: {
+    username: process.env.POSTGRES_USERNAME,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    host: process.env.POSTGRES_HOST,
+    dialect: 'postgres',
+  },
+  test: {
+    username: 'username',
+    database: 'binery',
+    password: 'password',
+    host: 'localhost',
+    dialect: 'postgres',
+  },
+  development: {
+    username: process.env.POSTGRES_USERNAME,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    host: process.env.POSTGRES_HOST,
+    dialect: 'postgres',
+    dialectOptions: {
+      ...dialectOptions,
+    },
+  },
+  staging: {
+    username: process.env.POSTGRES_USERNAME,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    host: process.env.POSTGRES_HOST,
+    dialect: 'postgres',
+    dialectOptions: {
+      ...dialectOptions,
+    },
+  },
+  production: {
+    username: process.env.POSTGRES_USERNAME,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    host: process.env.POSTGRES_HOST,
+    dialect: 'postgres',
+    dialectOptions: {
+      ...dialectOptions,
+    },
+  },
+};
+
+if (process.env.NODE_ENV === 'local') {
+  config = config.local;
+} else if (process.env.NODE_ENV === 'test') {
+  config = config.test;
+} else if (process.env.NODE_ENV === 'development') {
+  config = config.development;
+} else if (process.env.NODE_ENV === 'staging') {
+  config = config.staging;
+} else if (process.env.NODE_ENV === 'production') {
+  config = config.production;
+} else {
+  config = config.local;
+}
+
+if (process.env.NODE_ENV !== 'production') {
+  console.log(
+    `Running sequelize-cli on host: ${config.host}; db: ${config.database}`
+  );
+} else {
+  console.log('Running sequelize-cli in production');
+}
+
+module.exports = {
+  ...config,
+};
